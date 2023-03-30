e8401199638c444b4c58d4fbc78361d1e99e90d1
Admin <nenman.williams@gmail.com>
2023-03-30 20:42:09 +0100

    added models

--- wallet/models.py
+++ wallet/models.py
@@ -1,14 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
 
-class User(models.Model):
-    first_name = models.CharField(max_length=255, blank=False, null=False)
-    last_name = models.CharField(max_length=255, blank=False, null=False)
-    email = models.EmailField(blank=False, null=False)
-    password = models.CharField(max_length=255, blank=False)
+class Cards(models.Model):
+    card_name = models.CharField(max_length=255, blank=False, null=False)
+    card_number = models.IntegerField()
+    expiry_date = models.DateField()
+    cvv = models.IntegerField()
 
-    def __str__(self):
-        return self.first_name
 
+class Next_Of_Kin(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(blank=True, null=False)
+    phone_number = models.IntegerField(blank=False, null=False)
+    relationship = models.CharField(max_length=255, blank=False, null=False)
+    bvn = models.IntegerField()
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    next_0f_kin = models.ForeignKey(Next_Of_Kin, on_delete=models.CASCADE, related_name='next_of_kin')
+    home_address = models.CharField(max_length=255)
+    nin = models.IntegerField(blank=False, null=False)
+
+
+class wallet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    card = models.ForeignKey(Cards, on_delete=models.CASCADE, related_name='card')
