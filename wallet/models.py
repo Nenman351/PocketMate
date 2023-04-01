af22f5df3d6b72967595ee6fa28609992fdf3a5c
Admin <nenman.williams@gmail.com>
2023-04-01 21:49:46 +0100

    updated models

--- wallet/models.py
+++ wallet/models.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -10,6 +12,9 @@ class Card(models.Model):
     expiry_date = models.DateField()
     cvv = models.IntegerField()
 
+    def __str__(self):
+        return self.card_name
+
 
 class Next_Of_Kin(models.Model):
     full_name = models.CharField(max_length=255)
@@ -17,6 +22,9 @@ class Next_Of_Kin(models.Model):
     phone_number = models.IntegerField(blank=False, null=False)
     relationship = models.CharField(max_length=255, blank=False, null=False)
     bvn = models.IntegerField()
+
+    def __str__(self):
+        return self.full_name
 
 
 class User(AbstractUser):
@@ -29,3 +37,29 @@ class User(AbstractUser):
 class wallet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='card')
+
+
+class Status(Enum):
+    SUCCESSFUL = 'Successful'
+    PENDING = 'Pending'
+    FAILED = 'Failed'
+    REVERSED = 'Reversed'
+
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('Deposit', 'D'),
+        ('Withdrawal', 'W'),
+        ('Transfer', 'T'),
+    )
+    STATUS_CHOICES = [(status.name, status.value) for status in Status]
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_transactions')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_transactions')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=45, choices=STATUS_CHOICES, default='success')
+    transaction_type = models.CharField(max_length=50, choices=TRANSACTION_TYPES)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'Transaction #{self.pk}'
